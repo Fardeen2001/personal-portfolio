beb4d6b99ce7a969de82a49e4a08dd17ff44ecba
Fardeen Ahamed <1si20et403@sit.ac.in>
2023-10-06 23:22:29 +0530

    for email resend api installed

---
+++ components/EmailSection.jsx
@@ -0,0 +1,19 @@
+const EmailSection = () => {
+  return (
+    <section className="grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4">
+      <div>
+        <h5 className="text-xl font-bold text-white my-12">
+          Let&aposs Connect
+        </h5>
+        <p className="text-[#ADB7E] mb-4 max-w-md">
+          I&aposm currently looking for new opportunities, my inbox is always
+          open. Whether you have a question or just want to say hi, I&aposll try
+          my best to get back to you!
+        </p>
+        <div className="social flex flex-row gap-2"></div>
+      </div>
+    </section>
+  );
+};
+
+export default EmailSection;
