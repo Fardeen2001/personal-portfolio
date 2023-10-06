1d559d90c396878e42164601c470ca907fd74698
Fardeen Ahamed <1si20et403@sit.ac.in>
2023-10-06 22:30:05 +0530

    project section implemented

--- components/ProjectCard.jsx
+++ components/ProjectCard.jsx
@@ -1,13 +1,39 @@
-const ProjectCard = ({ imgUrl, title, description }) => {
+import { CodeBracketIcon, EyeIcon } from "@heroicons/react/24/outline";
+import Link from "next/link";
+
+const ProjectCard = ({ imgUrl, title, description, gitUrl, previewUrl }) => {
   return (
-    <div>
-      <div
-        className="h-52 md:h-72"
-        style={{ background: `url(${imgUrl})`, backgroundSize: "cover" }}
-      ></div>
-      <div className="text-white">
-        <h5>{title}</h5>
-        <p>{description}</p>
+    <div className="mt-5">
+      <div className="relative group">
+        <div
+          className="h-52 md:h-72 rounded-t-xl"
+          style={{
+            backgroundImage: `url(${imgUrl.src})`,
+            backgroundSize: "cover",
+          }}
+        ></div>
+        <div className="absolute top-0 left-0 w-full h-full bg-black bg-opacity-0 opacity-0 group-hover:bg-opacity-80 group-hover:opacity-100 transition-opacity duration-500">
+          <div className="absolute flex top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+            <Link
+              href={gitUrl}
+              target="_blank"
+              className="h-14 w-14 mr-2 border-2 rounded-full border-[#ADB7BE] hover:border-white text-[#ADB7BE] cursor-pointer group-hover:text-white flex items-center justify-center"
+            >
+              <CodeBracketIcon className="h-10 w-10" />
+            </Link>
+            <Link
+              href={previewUrl}
+              target="_blank"
+              className="h-14 w-14 border-2 rounded-full border-[#ADB7BE] hover:border-white text-[#ADB7BE] cursor-pointer group-hover:text-white flex items-center justify-center"
+            >
+              <EyeIcon className="h-10 w-10" />
+            </Link>
+          </div>
+        </div>
+      </div>
+      <div className="text-white rounded-b-xl mt-2 bg-[#181818] p-6">
+        <h5 className="text-xl font-semibold mb-2">{title}</h5>
+        <p className="text-[#ADB7BE]">{description}</p>
       </div>
     </div>
   );
--- components/ProjectSection.jsx
+++ components/ProjectSection.jsx
@@ -1,60 +1,130 @@
+"use client";
 import ProjectCard from "./ProjectCard";
+import img1 from "@/public/images/project/1.png";
+import img2 from "@/public/images/project/3.png";
+import img3 from "@/public/images/project/6.png";
+import img4 from "@/public/images/project/7.png";
+import img5 from "@/public/images/project/5.png";
+import img6 from "@/public/images/project/4.png";
+import img7 from "@/public/images/project/2.png";
+import ProjectTag from "./ProjectTag";
+import { useState } from "react";
 const projectData = [
   {
     id: 1,
     title: "E-Commerce Website",
     description: "project 1 next js",
-    image: "@/public/project/1.png",
-    tag: ["All", "Next js"],
+    image: img1,
+    gitUrl: "https://github.com/Fardeen2001/e-commerce-clothing-app",
+    previewUrl: "https://github.com/Fardeen2001/e-commerce-clothing-app",
+    tag: ["All", "Next Js"],
   },
   {
     id: 2,
     title: "E-Commerce Website",
     description: "project 2 react js",
-    image: "@/public/project/3.png",
-    tag: ["All", "React js"],
+    image: img2,
+    gitUrl: "https://github.com/Fardeen2001/E-Commerce_React_app",
+    previewUrl: "https://github.com/Fardeen2001/E-Commerce_React_app",
+    tag: ["All", "React Js"],
   },
   {
     id: 3,
     title: "Weather App",
     description: "project 3 weather js",
-    image: "@/public/project/6.png",
+    image: img3,
+    gitUrl: "https://github.com/Fardeen2001/weather-App",
+    previewUrl: "https://fardeen2001.github.io/weather-App/",
     tag: ["All", "Javascript"],
   },
   {
     id: 4,
     title: "Simple Spotify clone",
     description: "project 4 spotify",
-    image: "@/public/project/7.png",
+    image: img4,
+    gitUrl: "https://github.com/Fardeen2001/spotifyclone",
+    previewUrl: "https://fardeen2001.github.io/spotifyclone/",
     tag: ["All", "Javascript"],
   },
   {
     id: 5,
     title: "Random image portfolio",
     description: "project 5 css",
-    image: "@/public/project/5.png",
+    image: img5,
+    gitUrl: "https://github.com/Fardeen2001/grid-portfolio",
+    previewUrl: "https://fardeen2001.github.io/grid-portfolio/",
     tag: ["All", "CSS"],
   },
   {
     id: 6,
     title: "Usability hub landing page",
     description: "project 6 css",
-    image: "@/public/project4.png",
+    image: img6,
+    gitUrl: "https://github.com/Fardeen2001/spotifyclone",
+    previewUrl: "https://fardeen2001.github.io/spotifyclone/",
     tag: ["All", "CSS"],
   },
   {
     id: 7,
     title: "Glassomorpic Signup page",
     description: "project 7 css",
-    image: "@/public/project/2.png",
+    image: img7,
+    gitUrl: "https://github.com/Fardeen2001/spotifyclone",
+    previewUrl: "https://fardeen2001.github.io/spotifyclone/",
     tag: ["All", "CSS"],
   },
 ];
 const ProjectSection = () => {
+  const [tag, setTag] = useState("All");
+  const handleTagChange = (newTag) => {
+    setTag(newTag);
+  };
+
+  const filteredProjects = projectData.filter((item) => item.tag.includes(tag));
   return (
     <>
-      <h2>My Projects</h2>
-      <div>{}</div>
+      <h2 className="text-center text-4xl font-bold text-white mt-4">
+        My Projects
+      </h2>
+      <div className="text-white flex flex-row justify-center items-center gap-5 py-6">
+        <ProjectTag
+          onClick={handleTagChange}
+          name={"All"}
+          isSelected={tag === "All"}
+        />
+        <ProjectTag
+          onClick={handleTagChange}
+          name={"Next Js"}
+          isSelected={tag === "Next Js"}
+        />
+        <ProjectTag
+          onClick={handleTagChange}
+          name={"React Js"}
+          isSelected={tag === "React Js"}
+        />
+        <ProjectTag
+          onClick={handleTagChange}
+          name={"Javascript"}
+          isSelected={tag === "Javascript"}
+        />
+        <ProjectTag
+          onClick={handleTagChange}
+          name={"CSS"}
+          isSelected={tag === "CSS"}
+        />
+      </div>
+      <div>
+        {filteredProjects.map((item) => (
+          <ProjectCard
+            key={item.id}
+            title={item.title}
+            description={item.description}
+            imgUrl={item.image}
+            gitUrl={item.gitUrl}
+            previewUrl={item.previewUrl}
+          />
+        ))}
+      </div>
     </>
   );
 };
